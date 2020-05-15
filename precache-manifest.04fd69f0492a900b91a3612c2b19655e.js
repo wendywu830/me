8d34ded0c394b3166f2976dae628a002251ed741
Wendy Wu <wendywu@seas.upenn.edu>
2020-05-15 15:47:03 -0400

    Updates

---
+++ precache-manifest.04fd69f0492a900b91a3612c2b19655e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a714343703db90d58790",
+    "url": "/static/css/main.a63f52eb.chunk.css"
+  },
+  {
+    "revision": "a714343703db90d58790",
+    "url": "/static/js/main.a7143437.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b854740ee23cfdc59b55",
+    "url": "/static/css/2.102b480a.chunk.css"
+  },
+  {
+    "revision": "b854740ee23cfdc59b55",
+    "url": "/static/js/2.b854740e.chunk.js"
+  },
+  {
+    "revision": "63c4bc0d73eaaa8b02894760cd7eb721",
+    "url": "/index.html"
+  }
+];
